6f2c2d6e1c381657a06065e6b743840d770c0f2a
Luis Baldeon <lebaldeon@ext.vectoritcgroup.com>
2019-04-04 15:53:38 -0500

    Fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,21 +9,65 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor( private http: HttpClient ) { 
     this.cargarProductos();
   }
 
   private cargarProductos(){
-    this.http.get('https://angular-html-3c4a4.firebaseio.com/productos_idx.json')
+    return new Promise( (resolve, reject ) => {
+      this.http.get('https://angular-html-3c4a4.firebaseio.com/productos_idx.json')
         .subscribe( (resp: Producto[]) => {
-          console.log(resp);
           this.productos = resp;
           this.cargando = false;
+          resolve();
           
           //setTimeout(() => {
             //this.cargando = false;
           //}, 2000);
         });
+    });
+
+    
   }
+
+  getProducto( id: string ){
+    return this.http.get(`https://angular-html-3c4a4.firebaseio.com/productos/${ id }.json`)
+  }
+
+  buscarProducto( termino: string ){
+    if(this.productos.length === 0){
+      // cargar productos
+      this.cargarProductos().then( () => {
+        // ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    } else{
+      // aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+
+    //this.productosFiltrado = this.productos.filter( producto => {
+      //return true;
+    //});
+
+    //console.log( this.productosFiltrado );
+  }
+
+  private filtrarProductos ( termino: string ) {
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if( prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0 ){
+        this.productosFiltrado.push(prod);
+      }
+    });
+  }
+  
 }
