527f536b5a311ecee2da17e70f2ea32f36689f07
Luis Baldeon <lebaldeon@ext.vectoritcgroup.com>
2019-04-04 11:43:47 -0500

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,19 +9,30 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo : any[] = [];
 
   constructor( private http: HttpClient ) { 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    //console.log('Servicio de infoPagina listo');
-
+  private cargarInfo(){
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
 
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
+        });
+  }
+
+  private cargarEquipo(){
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-3c4a4.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+
+          this.equipo = resp;
           //console.log(resp);
-        })
+        });
   }
 }
